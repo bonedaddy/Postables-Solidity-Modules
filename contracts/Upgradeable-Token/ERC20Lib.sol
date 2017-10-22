abd39d1b4341ffb80e53aef0fa703407cc57f3f5
Postables <alextrottier2014@gmail.com>
2017-10-21 22:55:11 -0700

    Updates

---
+++ contracts/Upgradeable-Token/ERC20Lib.sol
@@ -0,0 +1,47 @@
+pragma solidity 0.4.18;
+
+contract ERC20Advanced {
+
+    event Transfer(address indexed _sender, address indexed _recipient, uint256 amount); 
+    event Approval(address indexed _owner, address indexed _spender, uint256 _amount);
+
+    function transfer(address _recipient, uint256 _amount)
+        public
+        returns (bool _transferred);
+
+    function transferFrom(address _owner, address _recipient, uint256 _amount)
+        public
+        returns (bool _transferredFrom);
+    
+    function approve(address _spender, uint256 _allowance)
+        public
+        returns (bool _approved);
+    
+    function burn(uint256 _amountBurn)
+        public
+        returns (bool _burned);
+    
+    /// @notice msg.sender must be admin, must be allowed to burn from that desired address
+    function burnFrom(address _burnFrom, uint256 _amountBurn)
+        public
+        returns (bool _burnedFrom);
+    
+    //GETTERS//
+
+    function balanceOf(address _addr)
+        public
+        view
+        returns (uint256 _balance);
+    
+    function totalSupply()
+        public
+        view
+        returns (uint256 _totalSupply);
+    
+    function allowance(address _owner, address _spender)
+        public
+        view
+        returns (uint256 _allowance);
+    
+  
+}
