ba9e84da5fbf04cfe5509bd5c49cb1b9cddfa92b
Postables <alextrottier2014@gmail.com>
2017-10-23 22:04:53 -0700

    a
    added docs to random num gen

--- libraries/Math/basicRandomNumberGenerator.sol
+++ libraries/Math/basicRandomNumberGenerator.sol
@@ -6,6 +6,8 @@ pragma solidity 0.4.18;
     * Description: VERY basic random number generator
     *              
  */
+
+ // @dev Import safemath
 import './SafeMath.sol';
 
 contract RandomTest {
@@ -13,11 +15,15 @@ contract RandomTest {
 
     uint256 public randomNum;
 
+    /// @dev Calls the random number generator
+    /// @param _max This is the number to use as the limit
     function generateRandomNumber(uint256 _max)
         public
         returns (bool _true)
     {
-        randomNum = randomGen(uint256(uint(keccak256(msg.sender, block.number))), _max);
+        // Assign "random" number
+        // Generate a "seed"
+        randomNum = randomGen(uint256(uint(keccak256(msg.sender, block.number, msg.sig, msg.gas))), _max);
         return true;
     }
     function randomGen(uint256 _seed, uint256 _max)
@@ -26,7 +32,15 @@ contract RandomTest {
         returns (uint256 randomNumber)
     {
         _max = _max.add(1);
+        // Uses the modulus operator to generate a sudo random number
         return uint(keccak256(block.blockhash(block.number-1), _seed ))%_max;
     }
 
+    function getRandomNumber()
+        public
+        view
+        returns (uint256 randomNumber)
+    {
+        return randomNum;
+    }
 }
