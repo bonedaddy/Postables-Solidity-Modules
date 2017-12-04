0f4bb659b4e796957efa2ed2dfce9d6bf0844cf6
Postables <alextrottier2014@gmail.com>
2017-12-03 21:39:39 -0800

    added SafeMath for ints

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Use at your own risk. Nothing in here is guaranteed to be in working order at an
 
 ## Math
 * BigInts
-* SafeMath
+* SafeMath (both signed and unsigned integer versions)
 * "Random" Number Generator
 
 ## Contract Control
---
+++ libraries/Math/SafeMathInt.sol
@@ -0,0 +1,27 @@
+pragma solidity 0.4.18;
+
+
+library SafeMath {
+
+  function mul(int256 a, int256 b) internal pure returns (int256) {
+    int256 c = a * b;
+    require(a == 0 || c / a == b);
+    return c;
+  }
+
+  function div(int256 a, int256 b) internal pure returns (int256) {
+    int256 c = a / b;
+    return c;
+  }
+
+  function sub(int256 a, int256 b) internal pure returns (int256) {
+    require(b <= a);
+    return a - b;
+  }
+
+  function add(int256 a, int256 b) internal pure returns (int256) {
+    int256 c = a + b;
+    require(c >= a);
+    return c;
+  }
+}
