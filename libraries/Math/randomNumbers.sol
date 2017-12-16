3610b2c3b52f4d6326ad3dc5c440cd76ef33deec
Postables <alextrottier2014@gmail.com>
2017-12-16 02:41:10 -0800

    upgraded random number generator
    Upgraded the random number generator to recalculate the seed each time a new number is generated by using the previous seed, the number that was generated, the sender of the msg, and the previous block hash.
    
    Seed is set to private.

---
+++ libraries/Math/randomNumbers.sol
@@ -0,0 +1,45 @@
+pragma solidity 0.4.18;
+
+import "./SafeMath.sol";
+
+/**
+    A random number with increasing randomness and less predictability the more it is used.
+*/
+
+contract RandomNumberGenerator {
+    using SafeMath for uint256;
+    
+    uint256     private    seed;
+    uint256     public      number;
+
+    function RandomNumberGenerator(uint256 _number, string _message) {
+        seed = uint256(keccak256(msg.sender, keccak256(_message), _number));
+    }
+    
+    function getRandomNumber(uint256 _max)
+        public
+    {
+        uint256 _number = generateRandomNumber(seed, _max);
+        seed = calculateNewSeed(seed, _number);
+        number = _number;
+    }
+
+    function calculateNewSeed(uint256 _seed, uint256 _number)
+        internal
+        view
+        returns (uint256)
+    {
+        return uint256(keccak256(_seed, _number,msg.sender,block.blockhash(block.number-1)));
+    }
+
+    function generateRandomNumber(uint256 _seed, uint256 _max)
+        internal
+        view
+        returns (uint256)
+    {
+        _max = _max.add(1);
+        // Uses the modulus operator to generate a sudo random number
+        uint256 randomNumber = uint256(keccak256(block.blockhash(block.number-1), _seed, msg.sender))%_max;
+        return randomNumber;
+    }
+}
