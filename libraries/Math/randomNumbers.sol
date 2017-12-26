4c875ae3505b9c7f5fc2c0eb1041f2a43ff13393
postables <alextrottier2014@gmail.com>
2017-12-26 15:07:56 -0800

    added contract version of the random number generator

--- libraries/Math/randomNumbers.sol
+++ libraries/Math/randomNumbers.sol
@@ -1,35 +1,36 @@
-pragma solidity 0.4.18;
+pragma solidity 0.4.19;
 
 import "./SafeMath.sol";
 
 /**
-    A random number with increasing randomness and less predictability the more it is used.
+    [Title]         Random Number Generator library
+    [Author]        Postables
+    [Description]   Library RNG contract, reusable seed pseudo-randomized upon usage.
 */
 
 contract RandomNumberGenerator {
     using SafeMath for uint256;
-    
-    uint256     private    seed;
+    uint256 private seed;
 
-    function RandomNumberGenerator(uint256 _number, string _message) {
-        seed = uint256(keccak256(msg.sender, keccak256(_message), _number, block.blockhash(block.number-1)));
+    function RandomNumberGenerator(string _msg, uint256 _number) {
+        seed = uint256(keccak256(_msg, msg.gas, tx.gasprice, msg.sender, _number));
     }
-    
+
     function getRandomNumber(uint256 _max)
         public
         returns (uint256)
     {
         uint256 _number = generateRandomNumber(seed, _max);
-        seed = calculateNewSeed(seed, _number);
+        require(calculateNewSeed(_max, _number));
         return _number;
     }
 
-    function calculateNewSeed(uint256 _seed, uint256 _number)
+    function calculateNewSeed(uint256 _max, uint256 _number)
         internal
-        view
-        returns (uint256)
+        returns (bool)
     {
-        return uint256(keccak256(_seed, _number,msg.sender,block.blockhash(block.number-1)));
+        seed = uint256(keccak256(_max, _number, msg.gas, tx.gasprice, msg.sender));
+        return true;
     }
 
     function generateRandomNumber(uint256 _seed, uint256 _max)
@@ -39,7 +40,7 @@ contract RandomNumberGenerator {
     {
         _max = _max.add(1);
         // Uses the modulus operator to generate a sudo random number
-        uint256 randomNumber = uint256(keccak256(block.blockhash(block.number-1), _seed, msg.sender))%_max;
+        uint256 randomNumber = uint256(keccak256(tx.gasprice, msg.gas, _seed, msg.sender))%_max;
         return randomNumber;
     }
 }
